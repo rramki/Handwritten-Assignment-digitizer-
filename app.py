25fab3691853460f0fcab2c5807c729a73dfa72a
rramki <ramki21in@gmail.com>
2026-02-22 12:24:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,52 @@
 import streamlit as st
 from PIL import Image
 import easyocr
+import numpy as np
+from pix2tex.cli import LatexOCR
+
 from transformers import TrOCRProcessor, VisionEncoderDecoderModel
 import torch
+
+@st.cache_resource
+def load_reader():
+    return easyocr.Reader(['en'], gpu=False)
+reader = load_reader()
+
+
+@st.cache_resource
+def load_trocr():
+    processor = TrOCRProcessor.from_pretrained(
+        "microsoft/trocr-small-handwritten")
+    
+    model = VisionEncoderDecoderModel.from_pretrained(
+        "microsoft/trocr-small-handwritten")
+    
+    model.eval()
+    return processor, model
+
+processor, model = load_trocr()
+
 
 st.set_page_config(page_title="Handwritten OCR Tool", layout="wide")
 st.title("📝 Handwritten Assignment → Text / LaTeX")
 
 uploaded_file = st.file_uploader("Upload Image", type=["png","jpg","jpeg"])
 
-@st.cache_resource
-def load_models():
-    reader = easyocr.Reader(['en'], gpu=False)
-    processor = TrOCRProcessor.from_pretrained("microsoft/trocr-small-handwritten")
-    model = VisionEncoderDecoderModel.from_pretrained("microsoft/trocr-small-handwritten")
-    return reader, processor, model
+# Pass numpy array to EasyOCR
+if uploaded_file is not None:
 
-if uploaded_file:
     image = Image.open(uploaded_file).convert("RGB")
-    st.image(image, use_column_width=True)
+    st.image(image)
 
-    reader, processor, model = load_models()
+    # Convert to numpy for EasyOCR
+    image_np = np.array(image)
 
-    st.write("🔍 Extracting Text...")
-    
-    # Normal Text OCR
-    text = reader.readtext(image, detail=0)
+    text = reader.readtext(image_np, detail=0)
     extracted_text = "\n".join(text)
 
+    st.text_area("Extracted Text", extracted_text)
+    
+    # Normal Text OCR
     st.subheader("📄 Extracted Text")
     st.text_area("Editable Text", extracted_text, height=200)
 
@@ -37,6 +56,15 @@ if uploaded_file:
     pixel_values = processor(images=image, return_tensors="pt").pixel_values
     generated_ids = model.generate(pixel_values)
     trocr_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+    
+    model = LatexOCR()
+    latex_result = model(image)
 
     st.subheader("✍ TrOCR Output")
     st.text_area("Handwritten Model Output", trocr_text, height=200)
+
+    st.subheader("🧮 Extracted LaTeX")
+    st.text_area("LaTeX Code", latex_result, height=200)
+
+    st.subheader("🔎 Rendered LaTeX")
+    st.latex(latex_result)

da0c6e941842ab6c145e8ab8d2bc69ebdb5bbeb8
rramki <ramki21in@gmail.com>
2026-02-23 19:08:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -47,4 +47,11 @@ if uploaded_file is not None:
     st.text_area("Editable Text", extracted_text, height=200)
 
 
-    
+
+from pix2tex.cli import LatexOCR
+st.title("📝 Handwritten Mathematical Equation → Text / LaTeX")
+uploaded_file2 = st.file_uploader("Upload Image", type=["png","jpg","jpeg"])
+model = LatexOCR()
+latex = model("uploaded_file2.png")
+
+print(latex)    
